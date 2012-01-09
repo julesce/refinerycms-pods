39b0ca53f31edcfbf05387c5da12f9e57f5c9985
James Thompson <james.b.thompson@gmail.com>
2012-01-09 14:48:04 +0200

    sorted out video pods

--- public/javascripts/refinerycms-pods.js
+++ public/javascripts/refinerycms-pods.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
 		controls: true
 	});
 
+  $(".video_pod .video a").each(function(index) {
+		var str = $(this).attr("href");
+	  	var yt_id = str.substring(str.lastIndexOf("/")+1, str.length);
+		$(this).attr("href", "http://www.youtube.com/embed/" + yt_id + "?rel=0");
+	  	$(this).html("<div class='video_img'><img src='http://img.youtube.com/vi/" + yt_id + "/0.jpg' /></div>");
+		$(this).html( $(this).html() + "<div class='video_play'></div>" );
+	});
+  
+	$(".video_pod .video a").colorbox({iframe:true, innerWidth:640, innerHeight:390, transition:"elastic"});
+
 });
